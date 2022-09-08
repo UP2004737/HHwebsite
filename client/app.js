4aa9976dee33c7e61b305c6a34f53a12ed1c23ae
UP2004737 <UP2004737@users.noreply.github.com>
2022-09-08 13:02:10 +0100

    Created simple API for website

--- client/app.js
+++ client/app.js
@@ -1,1 +1,12 @@
+async function loadMembers() {
+    let members;
+    const response = await fetch('members');
+    if (response.ok) {
+        members = await response.json();
+    } else {
+        members = [{msg: "Failed to load members"}];
+    }
+    return members;
+}
+
 let navLinks = document.getElementById("navLinks");
--- svr.js
+++ svr.js
@@ -1,5 +1,15 @@
 import express, {json} from 'express';
 
+let memberData = [
+    {name: "Max Little"}
+];
+
 const app = express();
 app.use(express.static('client'));
 app.listen(8080);
+
+async function getMembers(req, res) {
+    res.json(memberData);
+}
+
+app.get('/members', getMembers);
