82cb2950bf8cdbf7fb4701ef41eca0f364cd6e14
UP2004737 <UP2004737@users.noreply.github.com>
2022-09-12 13:42:13 +0100

    added new details

--- svr.js
+++ svr.js
@@ -2,7 +2,8 @@ import express, {json} from 'express';
 
 let memberData = [
     {name: "Max Little", course: "Software engineering", year: "Placement year", DoB: new Date(2001, 9, 29), funFact: "Big drinker"},
-    {name: "Natalie Geotenova", course: "Cyber security", year: "Third year", DoB: new Date(2001, 9, 12), funFact: "Bulgarian"}
+    {name: "Natalie Geotenova", course: "Cyber security", year: "Third year", DoB: new Date(2001, 9, 12), funFact: "Bulgarian"},
+    {name: "Ben Grave", course: "Computer science", year: "Graduated", DoB: new Date(2001, 1, 8), funFact: "He fucks Nati's mum so much he has a loyalty card for her"}
 ];
 
 const app = express();
